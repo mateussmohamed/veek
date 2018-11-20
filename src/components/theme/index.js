a368f997974145eb15d2873d6ec271752c3b829f
Mateus Santana <mateuss.dev@gmail.com>
2018-11-19 23:04:53 -0200

    Update theme config

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -4,50 +4,20 @@ import { normalize } from 'polished'
 export const GlobalStyle = createGlobalStyle`
   body, html {
     overflow-x: hidden;
+    font-family: 'Lato', sans-serif;
   }
+
   ${normalize()};
 `
 
 const colors = {
-  lightRed: '#ff4e50',
-  black: '#000000',
-  sunYellow: '#f9d423',
-  veryLightPink: '#f7f3f2',
-  greyishBrown: '#444444',
-  black0: 'rgba(0, 0, 0, 0)',
-  black60: 'rgba(0, 0, 0, 0.6)',
-  veryLightPinkTwo: '#cccccc',
-  brownGrey: '#999999',
-  cornFlower: '#667df3',
-  yellowIshorange: '#ffa710',
-  aqua: '#19d4ca',
-  brownishGrey: '#666666',
-  veryLightPinkThree: '#d8d8d8',
-  veryLightPinkFour: '#e3e3e3',
+  primary: '#eb5b49',
+  secondary: '#283583',
 }
 
 const typography = {
-  montserrat: '"Montserrat", sans-serif',
-  roboto: '"Roboto", sans-serif',
+  lato: '"Lato", sans-serif',
   buttonSize: '14px',
-
-  montserratStyle: {
-    'font-family': '"Montserrat", sans-serif',
-    'font-weight': 'normal',
-    'font-style': 'normal',
-    'font-stretch': 'normal',
-    'line-height': 'normal',
-    'letter-spacing': 'normal',
-  },
-
-  robotoStyle: {
-    'font-family': '"Montserrat", sans-serif',
-    'font-weight': 'normal',
-    'font-style': 'normal',
-    'font-stretch': 'normal',
-    'line-height': 'normal',
-    'letter-spacing': 'normal',
-  },
 }
 
 export default {
