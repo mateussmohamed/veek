3282045d77ae916f4655d775fa052e2a74028199
Mateus Santana <mateuss.dev@gmail.com>
2018-11-19 23:04:20 -0200

    Update Home component

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 
 import { Page } from 'templates'
-import { Atom } from 'atoms'
-import { Molecule } from 'molecules'
-import { Organism } from 'organisms'
 
 const Home = () => (
   <Page>
     <h1>Home Page</h1>
-    <Atom>I am a Atom</Atom>
-    <Molecule>I am a Molecule</Molecule>
-    <Organism>I am a Organism</Organism>
   </Page>
 )
 
