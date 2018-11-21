1f9a263458ec97c039bccbc2770088748b27802d
Mateus Santana <mateuss.dev@gmail.com>
2018-11-21 15:55:57 -0200

    Add HeroContent

---
+++ src/components/atoms/HeroContent/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+import { Grid } from 'react-styled-flexboxgrid'
+
+const HeroContent = styled(Grid)`
+  display: flex;
+  flex-direction: column;
+  max-width: 600px;
+`
+
+export default HeroContent
