852f2ca4bae5ec8c42df39e1a35c958973103465
Mateus Santana <mateuss.dev@gmail.com>
2018-11-21 19:25:12 -0200

    Adjust style HeroContent

--- src/components/atoms/HeroContent/index.js
+++ src/components/atoms/HeroContent/index.js
@@ -6,6 +6,13 @@ const HeroContent = styled(Grid)`
   display: flex;
   flex-direction: column;
   max-width: 600px;
+
+  @media only screen and (min-width: 0em) {
+    padding: 20px 0;
+  }
+  @media only screen and (min-width: 48em) {
+    padding: 200px 0;
+  }
 `
 
 export default HeroContent
