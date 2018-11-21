a72e61547e74fee595ba7930a7b64d4c5eb22072
Mateus Santana <mateuss.dev@gmail.com>
2018-11-21 19:24:08 -0200

    Add elements in DiscoverNews

--- src/components/organisms/DiscoverNews/index.js
+++ src/components/organisms/DiscoverNews/index.js
@@ -3,41 +3,48 @@ import styled from 'styled-components'
 
 import { Grid, Row, Col } from 'react-styled-flexboxgrid'
 
-import { Button } from 'atoms'
+import { Button, FullContainer } from 'atoms'
 import { Card } from 'molecules'
 
-const Container = styled(Grid)`
-  margin: 250px auto;
+const DiscoverNewsContainer = styled(Grid)`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding-bottom: 20px;
+  padding-top: 50px;
 `
 
 const DiscoverNews = () => (
-  <Container>
-    <Row>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-      <Col xs={12} sm={6} md={4}>
-        <Card />
-      </Col>
-    </Row>
-    <Row center="xs">
-      <Col>
-        <Button>Discover News</Button>
-      </Col>
-    </Row>
-  </Container>
+  <FullContainer>
+    <DiscoverNewsContainer>
+      <Row>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+        <Col xs={12} sm={6} md={4}>
+          <Card />
+        </Col>
+      </Row>
+      <Row center="xs">
+        <Col>
+          <Button>Discover News</Button>
+        </Col>
+      </Row>
+    </DiscoverNewsContainer>
+  </FullContainer>
 )
 
 export default DiscoverNews
