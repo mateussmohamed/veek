4a07bda3ebb81f8981717688d03490ce5aa39d6b
Mateus Santana <mateuss.dev@gmail.com>
2018-11-22 23:48:37 -0200

    Removing Navbar from Header

--- src/components/organisms/Header/index.js
+++ src/components/organisms/Header/index.js
@@ -1,44 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Grid, Row, Col } from 'react-styled-flexboxgrid'
 
-import { Button, Link, Logo } from 'atoms'
+import { Button, Logo, Link } from 'atoms'
+import { Navbar } from 'molecules'
 
 const HeaderStyled = styled.header`
-  width: 100%;
-  padding: 25px 0;
-
-  @media only screen and (min-width: 48em) {
+  @media only screen and (min-width: 0em) {
+    width: 100%;
     position: absolute;
     top: 0;
     left: 0;
+    padding: 25px 0;
   }
 `
+
 const HeaderLogo = styled(Link)`
-  @media only screen and (min-width: 0em) {
-    order: 0;
-    margin-bottom: 35px;
-    flex-basis: 100%;
-    text-align: center;
-  }
-  @media only screen and (min-width: 48em) {
+  @media only screen and (min-width: 64em) {
     order: 1;
     margin: 0;
-  }
-`
-
-const Navbar = styled.nav`
-  display: flex;
-
-  @media only screen and (min-width: 0em) {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
-  @media only screen and (min-width: 48em) {
-    flex-direction: row;
-    flex-wrap: inherit;
-    align-items: center;
-    justify-content: space-evenly;
   }
 `
 
@@ -50,9 +29,6 @@ const NavLink = styled(Link)`
 
   @media only screen and (min-width: 0em) {
     font-size: 16px;
-    margin-bottom: 25px;
-    flex-basis: 50%;
-    text-align: center;
   }
 
   @media only screen and (min-width: 48em) {
@@ -61,48 +37,34 @@ const NavLink = styled(Link)`
     text-align: normal;
   }
 `
+
 const ApplyButton = styled(Button)`
-  min-width: 100px;
+  min-width: 135px;
   order: 9;
 
-  @media only screen and (min-width: 0em) {
-    margin: 0 auto;
-    display: block;
-  }
-
-  @media only screen and (min-width: 48em) {
-    position: relative;
-    margin: 15px auto;
-  }
-
-  @media only screen and (min-width: 75em) {
+  @media only screen and (min-width: 64em) {
     position: absolute;
-    top: 4px;
-    right: -100px;
-    margin: 0;
+    right: -150px;
+  }
+  @media only screen and (min-width: 75em) {
+    right: -230px;
   }
 `
 
 const Header = () => (
   <HeaderStyled>
-    <Grid>
-      <Row>
-        <Col xs={12} style={{ position: 'relative' }}>
-          <Navbar>
-            <HeaderLogo>
-              <Logo color="#eb5b49" />
-            </HeaderLogo>
-            <NavLink>Team</NavLink>
-            <NavLink>About us</NavLink>
-            <NavLink>Process</NavLink>
-            <NavLink>Explication</NavLink>
-            <NavLink>New</NavLink>
-            <NavLink>Contact</NavLink>
-          </Navbar>
-          <ApplyButton secondary>Apply</ApplyButton>
-        </Col>
-      </Row>
-    </Grid>
+    <Navbar>
+      <HeaderLogo className="logo">
+        <Logo color="#eb5b49" />
+      </HeaderLogo>
+      <NavLink>Team</NavLink>
+      <NavLink>About us</NavLink>
+      <NavLink>Process</NavLink>
+      <NavLink>Explication</NavLink>
+      <NavLink>New</NavLink>
+      <NavLink>Contact</NavLink>
+      <ApplyButton secondary>Apply</ApplyButton>
+    </Navbar>
   </HeaderStyled>
 )
 
