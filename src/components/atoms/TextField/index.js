aabfa46bee4344b20a5387f045d1ccc535946997
Mateus Santana <mateuss.dev@gmail.com>
2018-11-23 01:20:29 -0200

    Update styles

--- src/components/atoms/TextField/index.js
+++ src/components/atoms/TextField/index.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 
-const baseStyle = css`
+const StyledTextField = styled.input`
   width: 100%;
   padding: 10px;
   border-radius: 8px;
   border: 1px solid #d5d5d5;
-`
-
-const inputStyle = css`
   color: #000;
   font-size: 18px;
   font-weight: bold;
@@ -19,31 +16,12 @@ const inputStyle = css`
     box-shadow: 1px 1px 10px 0px rgba(235, 91, 73, 0.5);
     outline: 0;
   }
-
-  &:-webkit-autofill,
-  &:-webkit-autofill:hover,
-  &:-webkit-autofill:focus,
-  &:-webkit-autofill:active {
-    -webkit-text-fill-color: #000;
-    box-shadow: 0 0 0px 1000px #fff inset;
-    transition: 'color 9999s ease-out, background-color 9999s ease-out';
-  }
 `
 
-const StyledTextField = styled.input`
-  ${baseStyle};
-  ${inputStyle};
-`
+const TextField = props => {
+  const textField = React.createRef()
 
-class TextField extends Component {
-  constructor(props) {
-    super(props)
-    this.input = React.createRef()
-  }
-
-  render() {
-    return <StyledTextField {...this.props} ref={this.input} />
-  }
+  return <StyledTextField {...props} ref={textField} />
 }
 
 TextField.defaultProps = {
