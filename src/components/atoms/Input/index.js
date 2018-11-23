ae7b232a18456cae345ff5c4fb13ed1d4e1d741f
Mateus Santana <mateuss.dev@gmail.com>
2018-11-23 01:20:20 -0200

    Update propTypes

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -14,17 +14,12 @@ const getComponent = props => ({
 const Input = props => getComponent(props)[props.type]
 
 Input.defaultProps = {
-  multiline: false,
   type: 'text',
   defaultValue: '',
 }
 
 Input.propTypes = {
-  multiline: PropTypes.bool,
   type: PropTypes.string,
-  value: PropTypes.string,
-  name: PropTypes.string,
-  onChange: PropTypes.func,
 }
 
 export default Input
