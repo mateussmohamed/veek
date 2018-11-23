89522b50a8d810dd0824a3c08ce98ab8b935f57d
Mateus Santana <mateuss.dev@gmail.com>
2018-11-22 23:45:46 -0200

    Refac Icon

--- src/components/atoms/Icon/index.js
+++ src/components/atoms/Icon/index.js
@@ -1,32 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 
 import Facebook from './Facebook'
 import Twitter from './Twitter'
 import Instagram from './Instagram'
 import Linkedin from './Linkedin'
 import Clock from './Clock'
+import Menu from './Menu'
 
-const ICONS = {
-  facebook: <Facebook />,
-  twitter: <Twitter />,
-  instagram: <Instagram />,
-  linkedin: <Linkedin />,
-  clock: <Clock />,
-}
+const getIcon = props => ({
+  facebook: <Facebook {...props} />,
+  twitter: <Twitter {...props} />,
+  instagram: <Instagram {...props} />,
+  linkedin: <Linkedin {...props} />,
+  clock: <Clock {...props} />,
+  menu: <Menu {...props} />,
+})
 
-const IconStyled = styled.i`
-  svg {
-    path {
-      :hover {
-        box-shadow: 2px 3px 6px 0 #000;
-      }
-    }
-  }
-`
-
-const Icon = props => <IconStyled>{ICONS[props.name]}</IconStyled>
+const Icon = props => getIcon(props)[props.name]
 
 Icon.propTypes = {
   name: PropTypes.string.isRequired,
