9d2624de65730977ecbea2f9f230db9adb4ddf45
Mateus Santana <mateuss.dev@gmail.com>
2018-11-22 23:48:54 -0200

    Add backgroud images

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -2,18 +2,38 @@ import { createGlobalStyle } from 'styled-components'
 import { normalize } from 'polished'
 
 export const GlobalStyle = createGlobalStyle`
-  body, html {
-    overflow-x: hidden;
-    font-family: 'Lato', sans-serif;
-    box-sizing: border-box;
-
-  }
+  ${normalize()};
 
   *, *:before, *:after {
     box-sizing: inherit;
   }
 
-  ${normalize()};
+   body, html {
+    overflow-x: hidden;
+    font-family: 'Lato', sans-serif;
+    box-sizing: border-box;
+  }
+
+
+  html {
+    @media only screen and (min-width: 0em) {
+      background-image: url(/assets/bg_left.png);
+      background-position: -170px 200px;
+      background-repeat: no-repeat;
+    }
+
+    @media only screen and (min-width: 48em) {
+      background-position: -110px 200px;
+    }
+  }
+
+  body {
+    @media only screen and (min-width: 64em) {
+      background-image: url(/assets/bg_right.png);
+      background-position: 115% 110px;
+      background-repeat: no-repeat;
+    }
+  }
 `
 
 const colors = {
